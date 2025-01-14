537cd9b4ef3e3aa7afe5954b0655eb0b5e0dc365
t2023-m0028 <t2023-m0028@t2023-m0028ui-MacBookAir.local>
2025-01-14 18:32:47 +0900

    refactor : 메뉴도  Kiosk 클래스에서 관리하도록 수정

--- src/main/java/com/example/kioskLevel3/Kiosk.java
+++ src/main/java/com/example/kioskLevel3/Kiosk.java
@@ -1,16 +1,24 @@
 package com.example.kioskLevel3;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 public class Kiosk {
-    private final List<MenuItem> menuItems;
+    private final List<MenuItem> menuItems = new ArrayList<>();
     private boolean isRunning;
 
-    public Kiosk(List<MenuItem> menuItems) {
-        this.menuItems = menuItems;
+    public Kiosk() {
+        addMenuItem();
         this.isRunning = true;
+    }
+
+    public void addMenuItem() {
+        menuItems.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
     }
 
     public void start() {   // kiosk2/Main 클래스의 20번째 줄 부터 같음

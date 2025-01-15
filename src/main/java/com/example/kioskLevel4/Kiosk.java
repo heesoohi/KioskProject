4cb2e7bb8d37557a9defb6977d4b0d4f76a5b025
이희수 <t2023-m0028@t2023-m0028ui-MacBookAir.local>
2025-01-15 10:21:10 +0900

    feat: level 4 - 메뉴 아이템을 관리하는 메뉴 클래스 생성

---
+++ src/main/java/com/example/kioskLevel4/Kiosk.java
@@ -0,0 +1,4 @@
+package com.example.kioskLevel4;
+
+public class Kiosk {
+}
---
+++ src/main/java/com/example/kioskLevel4/Menu.java
@@ -0,0 +1,63 @@
+package com.example.kioskLevel4;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Menu {
+    // MenuItem 클래스를 관리하는 클래스
+    // 버거, 음료 등 각 카테고리 내에 여러 MenuItem 을 포함
+    private final String categoryName;
+    private final List<MenuItem> menuItems = new ArrayList<>();
+
+    public Menu(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void addMenuItem(MenuItem item) {
+        menuItems.add(item);
+    }
+
+    public void printMenu() {
+        boolean isRunning =true;
+        while(true) {
+            System.out.println("[ " + categoryName.toUpperCase() + " MENU ]");
+            for (int i = 0; i < menuItems.size(); i++) {
+                MenuItem item = menuItems.get(i);
+                System.out.println((i + 1) + ". " + item.getName() + " | W " + item.getPrice() + " | " + item.getDescription());
+            }
+            System.out.println("0. 뒤로 가기");
+
+            System.out.println("\n원하는 메뉴의 번호를 입력하세요");
+
+            Scanner scanner = new Scanner(System.in);
+
+            try {
+                int choice = scanner.nextInt();
+
+                // 입력된 번호에 따라 로직 실행
+                if (choice == 0) {
+                    return;
+                } else if (choice > 0 && choice <= menuItems.size()) {
+                    // 선택된 메뉴의 인덱스 가져오기
+                    MenuItem selectedItem = menuItems.get(choice - 1);
+                    // 해당 메뉴의 상세 정보 출력
+                    System.out.println(choice + "." + selectedItem.getName() + " | W " + selectedItem.getPrice() + " | " + selectedItem.getDescription());
+                    isRunning = false;
+                } else {
+                    System.out.println("\n잘못된 입력입니다. 다시 시도하세요.");
+                }
+            } catch (InputMismatchException e) {
+                // 숫자가 아닌 값을 입력했을 경우 예외 처리
+                System.out.println("\n숫자만 입력해주세요.");
+                scanner.nextLine(); // 사용자가 숫자가 아닌 값을 입력했을 때 버퍼에 남아있는 잘못된 값을 처리하고, 새로운 입력 받기
+            }
+        }
+    }
+
+}

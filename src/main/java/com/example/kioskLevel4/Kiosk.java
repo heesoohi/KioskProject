b3ca6902322003518b5cf962c4524002ddf38b48
이희수 <leehsdd33@gmail.com>
2025-01-15 10:56:59 +0900

    feat : level 4 - 메뉴 아이템을 관리하는 메뉴 클래스 생성

--- src/main/java/com/example/kioskLevel4/Kiosk.java
+++ src/main/java/com/example/kioskLevel4/Kiosk.java
@@ -1,4 +1,45 @@
 package com.example.kioskLevel4;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
 public class Kiosk {
+    private final List<Menu> menus = new ArrayList<>();
+    private boolean isRunning;
+
+    public void add(Menu menu) {
+        menus.add(menu);
+    }
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        isRunning = true;
+
+        while (isRunning) {
+            System.out.println("[ MAIN MENU ]");
+            for (int i = 0; i < menus.size(); i++) {
+                System.out.println((i + 1) + ". " + menus.get(i).getCategoryName());
+            }
+            System.out.println("0. 종료");
+
+            try{
+                System.out.println("원하는 메뉴 번호를 선택하세요: ");
+                int mainChoice = scanner.nextInt();
+
+                if (mainChoice == 0 ) {
+                    System.out.println("프로그램을 종료합니다.");
+                    isRunning = false;
+                } else if (mainChoice > 0 && mainChoice <= menus.size()) {
+                    menus.get(mainChoice-1).printMenu();
+                } else {
+                    System.out.println("잘못된 입력입니다. 다시 시도하세요.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("숫자만 입력해주세요.");
+                scanner.nextLine();
+            }
+        }
+    }
 }
